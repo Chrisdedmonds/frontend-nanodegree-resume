d145c19a873037ae9bd0aa8b02144d106e7f8ff8
Chrisdedmonds <chris.edmonds128@gmail.com>
2016-04-26 07:51:30 -0400

    start

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Chris Edmonds");
+var name = 'Chris Edmonds';
+var role = 'Web Developer';
+
+var formattedName = HTMLheaderName.replace('%data%', name);
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+
