3c44eb980069bb90caa7e011d3e98a26fd529d29
Chrisdedmonds <chris.edmonds128@gmail.com>
2016-04-27 00:20:59 -0400

    cb

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,27 @@ var formattedRole = HTMLheaderRole.replace('%data%', role);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
 
+var bio = {
+    "name": "Chris",
+    "role": "developer",
+    "contact_info": 6103221707,
+    "welcome_message": "Hey, hey!"
+}
+
+$('#header').append(bio.welcome_message);
+
+var work = {};
+var education = {};
+
+work["employer"] = "Booz";
+var education = 
+[{
+    "name": "Williams",
+    "year": 2011
+},
+{      
+    "onlineCourses": ["Java", "Python", "JavaScript"]
+}]
+
+$('#main').append(work['employer']);
+$('#main').append(education[0].name);
